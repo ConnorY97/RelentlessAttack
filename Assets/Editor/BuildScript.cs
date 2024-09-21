5d1c947daa8221b6659e31cb23b48c971c68d745
Connor Young <c_young97@live.com>
2024-09-21 18:12:11 +1000

    Updating build command

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -37,10 +37,36 @@ public class Builder
         }
     }
 
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] {
+                "Assets/Scenes/MainMenu.unity",
+                "Assets/Scenes/Soldier.unity",
+                "Assets/Scenes/Sniper.unity",
+            };
+        buildPlayerOptions.locationPathName = "../LinuxBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
     // This function will be called from the build process
     public static void build()
     {
         // Build EmbeddedLinux ARM64 Unity player
-        BuildEmbeddedLinux(EmbeddedLinuxArchitecture.Arm64);
+        MyBuild();
     }
 }
