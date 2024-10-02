36da8d3553b77c82957473fb04d10ae7b06cd649
Connor Young <c_young97@live.com>
2024-10-02 12:00:51 +1000

    Adding a build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,39 +4,6 @@ using UnityEngine;
 
 public class Builder
 {
-    private static void BuildEmbeddedLinux(EmbeddedLinuxArchitecture architecture)
-    {
-        // Set architecture in BuildSettings
-        EditorUserBuildSettings.selectedEmbeddedLinuxArchitecture = architecture;
-
-        // Setup build options (e.g. scenes, build output location)
-        var options = new BuildPlayerOptions
-        {
-            // Change to scenes from your project
-            scenes = new[]
-            {
-                "Assets/Scenes/MainMenu.unity",
-                "Assets/Scenes/Soldier.unity",
-                "Assets/Scenes/Sniper.unity",
-            },
-            // Change to location the output should go
-            locationPathName = "../EmbeddedLinuxPlayer/",
-            options = BuildOptions.CleanBuildCache | BuildOptions.StrictMode,
-            target = BuildTarget.EmbeddedLinux
-        };
-
-        var report = BuildPipeline.BuildPlayer(options);
-
-        if (report.summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log($"Build successful - Build written to {options.locationPathName}");
-        }
-        else if (report.summary.result == BuildResult.Failed)
-        {
-            Debug.LogError($"Build failed");
-        }
-    }
-
     public static void MyBuild()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
